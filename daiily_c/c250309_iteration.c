f445fc815e3fa9b4a6b57a647433792dd3f92e87
sharpscar <sharpscar@gmail.com>
2025-03-10 12:35:15 +0900

    250310 점심시간

---
+++ daiily_c/c250309_iteration.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+// 두 수중 더 큰수를 리턴한다.
+void play_for();
+void rplay_for();
+void play_while();
+void r_play_while();
+
+
+int main(){
+    
+    // for문으로 1-10
+    // play_for();
+    // for문으로 10-1
+    // rplay_for();
+    //여기는 메인 공간 입니다.
+    // while문으로 1-10
+    // play_while();
+    // while문으로 10-1
+    r_play_while();
+
+    return 0;
+}
+void play_while()
+{
+    int cnt=1;
+    while(cnt<=10)
+    {
+        
+        printf("%d\n", cnt);
+        cnt++;
+    }
+}
+
+void r_play_while()
+{
+    int cnt=10;
+    while(cnt>0)
+    {
+        
+        printf("%d\n", cnt);
+        cnt--;
+    }
+}
+
+
+void play_for()
+{
+    for (int i=0; i<11; i++)
+    {
+        printf("%d\n",i);
+    }
+}
+
+void rplay_for()
+{
+    for (int i=10; i>0; i--)
+    {
+        printf("%d\n",i);
+    }
+}
