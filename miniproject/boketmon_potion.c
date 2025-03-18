0e7ad201c03d60e00aad199015cf3b4e590aaaeb
sharpscar <sharpscar@gmail.com>
2025-03-19 08:47:15 +0900

    응? 기능추가했다고?

--- miniproject/boketmon_potion.c
+++ miniproject/boketmon_potion.c
@@ -12,14 +12,16 @@ int monster_appear(int *,int ,int);
 void introduce_apear_monster(int*);
 int* battle_boss_monster(int * ,int * ,int );
 int* battle_normal_monster(int *, int*, int);
+void call_a_song();
 int hit_hp(int hp, int ad);
 // int* update_status(int *, int ,int);         // 전투 후 복켓몬의 상태를 업데이트한다.
 // float calc_exp(my_exp);
 bool player_die = false;
+
 int main()
 {
-    
-    int static potion[30]={1,}; //층수 / 포션 유무
+    // potion[30] = {1,}; 구문이 안먹어서 울며겨자먹기 타이핑!
+    int  potion[30]={1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1}; //층수 / 포션 유무
     int is_boss=0;
     int cnt_battle=0;
     int c_tier=1;
@@ -100,6 +102,7 @@ int* battle_normal_monster(int *my_status,int *potion,int c_tier)
     int mon_ad = mon_lv *0.6;
     int pick = my_status[0];
     int my_lv = my_status[1];
+    int init_hp = my_status[2];
     int my_hp = my_status[2];
     int my_ad = my_status[3]; 
     int my_exp = my_status[4]; 
@@ -115,27 +118,169 @@ int* battle_normal_monster(int *my_status,int *potion,int c_tier)
     
         printf("[System!] %d 층의 %d 턴이 시작되었습니다..복켓몬의 렙은 %d 몬스터의 렙은 %d입니다.\n",c_tier, turn +1, my_lv, mon_lv); //추후 시스템 프린트 함수로 묶어서 출력
         printf("===================================================================================\n");
-        // user의 hp가 0밑으로 떨어지거나 몬스터의 hp가 0으로 떨어지면 게임 종료
+
+        //굳이 선택지를 넣으려면 여기 넣어야한다.
+        printf("행동을 선택하세요.\n");
+        printf("1.공격한다 2.물약을 마신다[있다면] 3.기술[있다면]\n");
+        scanf("%d", &answer);
+        if(answer==2){            
+            if(potion[c_tier-1]==0)
+            {
+                printf("아쉽지만 물약이 없어 아무행동도 하지않습니다 -이번층은 틀렸어\n");
+            }else{
+                printf("물약을 마십니다.. %d\n", potion[c_tier-1]);            
+                my_hp = init_hp;
+                potion[c_tier-1]= 0;//으로 초기화
+            }
+        }else if(answer==3)
+        {
+            //스킬이 있다면 스킬을 수행!  아직은 아무것도 하지 않는다.
+            printf("기술을 시전했으나 아직 게으른 개발자가 머리를짜내지 않았습니다.");
+            //아쉬운대로 포켓몬 주제가라도 불러봅니다.
+            call_a_song(); 
+        }else if(answer==1)
+        {
+            // user의 hp가 0밑으로 떨어지거나 몬스터의 hp가 0으로 떨어지면 게임 종료
         mon_hp = hit_hp(mon_hp, my_ad);
         printf("[System!] 복켓몬이 상대몬스터에게 %d만큼의 피해를 입혔습니다, 몬스터의 hp는 %d입니다..\n",my_ad,mon_hp); //추후 시스템 프린트 함수로 묶어서 출력
         my_hp = hit_hp(my_hp, mon_ad);
         printf("[System!] 몬스터가 복켓몬 몬스터에게 %d만큼의 피해를 입혔습니다, 복켓몬의 hp는 %d입니다.\n",mon_ad,my_hp);
         printf("===================================================================================\n");        
-        printf("                                                                    턴을 넘기려면 Enter!\n");
-        // 해당층에서 1번만 주어지는 혜택을 사용하겠는가?
-        if(potion[c_tier]==1)
+        
+        if(mon_hp<=0)
         {
-            //사용자 입력 받는곳 추후 potion의 배열에 1이 있으면 으로 조건문을 삽입 if (my_hp <=50%) &&(potion[c_tier] == 1) 
-            printf("복켓몬의 hp가 %d입니다. 회복을원하시면 (h)eal을 입력하세요 ) :\n",my_hp);
-            scanf("%c",&answer);
-            if(answer = 'y')
-            {
-                //복켓몬의 hp가 my_status[2]여기에 저장되어이 있던 수치 그대로 다시 조정
-                my_hp = my_status[2];
-                //그리고 potion[c_tier] = 0 으로 설정
-                potion[c_tier] = 0;
+            //유저가 이긴상황이다.
+            printf("유저 복켓몬이 미약한 힘으로 %d 턴만에 상대 몬스터를 제압했습니다. 남은 hp는 %d입니다.\n",turn+1, my_hp);
+
+            // 30~50 렌덤하게 수치를 넣는다. 100이 넘어가면 레벨이 1씩증가하고 나머지를 exp에저장하는 구조가 좋을듯
+            gain_exp = (30 +rand() %50);
+            printf("[System!] 현재 경험치는 %d이며 획득 경험치는 %d입니다.\n",my_exp, gain_exp);
+
+            if((my_exp+gain_exp)>=100){           
+                levelup  = (my_exp+gain_exp) /100; // 레벨업 숫자
+                gain_exp = (my_exp+gain_exp) % 100; // 남은 exp    
+                printf("[System!] 축하합니다. Level up!\n"); 
+                //레벨업 했을때에만 수치의 변화가 생겨야한다. 그 외에는 변화가 있어선 안된다.
+                my_status[1] = my_status[1] + levelup;
+                my_status[2] = my_status[1] * 25;
+                my_status[3] = my_status[1] *3.5;
+                my_status[4] = gain_exp;
+              
+            } else {
+                //레벨업을 안한 상태이다.
+                levelup = 0;
+                my_exp = my_exp + gain_exp;         // 경험치만 추가된상태이다.       
+                my_status[4] = my_exp;
             }
+            return my_status;
+
+            break;
+        }else if(my_hp<=0)
+        {
+            
+            // 사실 포션을 이용해서 부활시켜야할거같다.  c_tier 정보도 리셋해야한다.
+            printf("유저 복켓몬이 상대 몬스터에게 %d턴만에 제압당하여 갖고있는 돈과 명예를 모두 잃었습니다 -game over 몬스터의 남은 체력 :%d",turn, mon_hp);
+            // 리셋후 게임재시작은 추후구현
+            printf("게임을 다시 진행하시겠습니까?");
+            scanf("%c", &answer);
+            if(answer='y')
+            {
+                my_status = choose_my_monster();
+                c_tier=1; // 
+                return my_status;
+            }
+
+            break;
         }
+
+        scanf("",&answer); //엔터를 쳐야 턴을 넘기는 기능
+        getchar();
+
+        turn++;
+        }
+
+        
+    }
+
+    return my_status;
+}
+
+int hit_hp(int hp, int ad)
+{
+    int health_point;
+    health_point = hp-ad;
+    return health_point;
+}
+
+int* battle_boss_monster(int *my_status,int *potion,int c_tier)
+{
+    int gain_exp,randint;
+    int turn=0;
+    char answer;
+    int levelup=0;   
+
+    
+    // 보스몬스터레벨= 층*2
+    int mon_lv =c_tier *2;
+    // 몬스터의 hp
+    int mon_hp = mon_lv*7;
+    //공격력
+    int mon_ad = mon_lv *0.6;      
+    char my_monsters[][20]={"이상해씨","파이리","꼬부기","피카츄"};
+
+    char monsters_name[][20]= {"이상해씨","이상해풀","메가리자몽","메가거북왕","메가독침붕","피죤투","아보크","모래두지","니드리나","식스테일","나인테일","골뱃","냄새꼬","파라스","파라섹트","도나리","디그다","닥트리오","페르시온","페르시온","고라파덕","성원숭","슈륙챙이","메가후딘","괴력몬","우츠동","데구리","딱구리","야돈","야도란"};
+    int pick =  my_status[0];
+    int my_lv = my_status[1];
+    int init_hp =my_status[2];
+    int my_hp = my_status[2];
+    int my_ad = my_status[3]; 
+    int my_exp = my_status[4]; 
+    srand((unsigned int)time(NULL));
+    
+    printf("<<%s 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",my_monsters[pick-1],my_lv, my_hp, my_ad);
+    printf("<<트레이너의 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",c_tier*2, mon_hp*2, mon_ad*2);
+    printf("<< %s 의 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",monsters_name[c_tier-1],mon_lv, mon_hp, mon_ad);
+    while(1)
+    {     
+    
+        printf("===================================================================================\n");
+    
+        printf("[System!] %d 층의 %d 턴이 시작되었습니다..복켓몬의 렙은 %d 몬스터의 렙은 %d입니다.\n",c_tier, turn +1, my_lv, mon_lv); //추후 시스템 프린트 함수로 묶어서 출력
+        printf("===================================================================================\n");
+
+        //굳이 선택지를 넣으려면 여기 넣어야한다.
+        printf("행동을 선택하세요.\n");
+        printf("1.공격한다 2.물약을 마신다[있다면] 3.기술[있다면]\n");
+        scanf("%d", &answer);
+        if(answer==2){
+
+            if(potion[c_tier-1]==0)
+            {
+                printf("아쉽지만 물약이 없어 아무행동도 하지않습니다 -이번층은 틀렸어\n");
+            }else{
+                printf("물약을 마십니다.. %d\n", potion[c_tier-1]);            
+                my_hp = init_hp;
+                potion[c_tier-1]= 0;//으로 초기화
+            }
+            // potion 배열의 [c_tier-1]인덱스의 값을 0으로 변경 - 해당층의 물약은 이제없다 다시 마실수 없도록 
+        }else if(answer==3)
+        {
+            //스킬이 있다면 스킬을 수행!  아직은 아무것도 하지 않는다.
+            printf("기술을 시전했으나 아직 게으른 개발자가 머리를짜내지 않았습니다.");
+             //아쉬운대로 포켓몬 주제가라도 불러봅니다.
+            call_a_song(); 
+        
+        }else if(answer==1)
+        {
+            // user의 hp가 0밑으로 떨어지거나 몬스터의 hp가 0으로 떨어지면 게임 종료
+        mon_hp = hit_hp(mon_hp, my_ad);
+        printf("[System!] 복켓몬이 상대몬스터에게 %d만큼의 피해를 입혔습니다, 몬스터의 hp는 %d입니다..\n",my_ad,mon_hp); //추후 시스템 프린트 함수로 묶어서 출력
+        my_hp = hit_hp(my_hp, mon_ad);
+        printf("[System!] 몬스터가 복켓몬 몬스터에게 %d만큼의 피해를 입혔습니다, 복켓몬의 hp는 %d입니다.\n",mon_ad,my_hp);
+        printf("===================================================================================\n");        
+        
+        
+     
 
         if(mon_hp<=0)
         {
@@ -187,120 +332,9 @@ int* battle_normal_monster(int *my_status,int *potion,int c_tier)
         getchar();
 
         turn++;
-    }
+        }
 
-    return my_status;
-}
-
-int hit_hp(int hp, int ad)
-{
-    int health_point;
-    health_point = hp-ad;
-    return health_point;
-}
-
-int* battle_boss_monster(int *my_status,int *potion,int c_tier)
-{
-    int gain_exp,randint;
-    int turn=0;
-    char answer;
-    int levelup=0;   
-
-    
-    // 보스몬스터레벨= 층*2
-    int mon_lv =c_tier *2;
-    // 몬스터의 hp
-    int mon_hp = mon_lv*7;
-    //공격력
-    int mon_ad = mon_lv *0.6;      
-    char my_monsters[][20]={"이상해씨","파이리","꼬부기","피카츄"};
-
-    char monsters_name[][20]= {"이상해씨","이상해풀","메가리자몽","메가거북왕","메가독침붕","피죤투","아보크","모래두지","니드리나","식스테일","나인테일","골뱃","냄새꼬","파라스","파라섹트","도나리","디그다","닥트리오","페르시온","페르시온","고라파덕","성원숭","슈륙챙이","메가후딘","괴력몬","우츠동","데구리","딱구리","야돈","야도란"};
-    int pick =  my_status[0];
-    int my_lv = my_status[1];
-    int my_hp = my_status[2];
-    int my_ad = my_status[3]; 
-    int my_exp = my_status[4]; 
-    srand((unsigned int)time(NULL));
-    
-    printf("<<%s 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",my_monsters[pick-1],my_lv, my_hp, my_ad);
-    printf("<<트레이너의 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",c_tier*2, mon_hp*2, mon_ad*2);
-    printf("<< %s 의 레벨은 %d이며 현재 hp는 %d 공격력은 %d 입니다.>>\n",monsters_name[c_tier-1],mon_lv, mon_hp, mon_ad);
-    while(1)
-    {     
-
-
-        printf("[System!] %d 층의 %d 턴이 시작되었습니다..복켓몬의 렙은 %d 몬스터의 렙은 %d입니다.\n",c_tier, turn +1, my_lv, mon_lv); //추후 시스템 프린트 함수로 묶어서 출력        
-        // user의 hp가 0밑으로 떨어지거나 몬스터의 hp가 0으로 떨어지면 게임 종료
-        mon_hp = hit_hp(mon_hp, my_ad);
-        printf("[System!]유저의 복켓몬이 상대몬스터에게 %d만큼의 피해를 입혔습니다, 보스몬스터의 hp는 %d입니다..\n",my_ad,mon_hp); //추후 시스템 프린트 함수로 묶어서 출력
-        my_hp = hit_hp(my_hp, mon_ad);
-        printf("[System!]보스몬스터가 유저의 복켓몬에게 %d만큼의 피해를 입혔습니다, 복켓몬의 hp는 %d입니다.\n",mon_ad,my_hp);
-        printf("                                                                    턴을 넘기려면 Enter!\n");
         
-        // 해당층에서 1번만 주어지는 혜택을 사용하겠는가?
-        if(potion[c_tier]==1)
-        {
-            //사용자 입력 받는곳 추후 potion의 배열에 1이 있으면 으로 조건문을 삽입 if (my_hp <=50%) &&(potion[c_tier] == 1) 
-            printf("복켓몬의 hp가 %d입니다. 회복을원하시면 (h)eal을 입력하세요 ) :\n",my_hp);
-            scanf("%c",&answer);
-            if(answer = 'h')
-            {
-                //복켓몬의 hp가 my_status[2]여기에 저장되어이 있던 수치 그대로 다시 조정
-                my_hp = my_status[2];
-                //그리고 potion[c_tier] = 0 으로 설정
-                potion[c_tier] = 0;
-            }
-        }
-
-        if(mon_hp<=0)
-        {
-            //유저가 이긴상황이다.
-            printf("유저의 복켓몬이 미약한 힘으로 %d 턴만에 상대 몬스터를 제압했습니다. 남은 hp는 %d입니다.\n",turn+1, my_hp);
-
-            // 30~50 렌덤하게 수치를 넣는다. 100이 넘어가면 레벨이 1씩증가하고 나머지를 exp에저장하는 구조가 좋을듯
-            gain_exp = (50 +rand() %70);
-            printf("[System!] 현재 경험치는 %d이며 획득 경험치는 %d입니다.\n",my_exp, gain_exp);
-
-            if((my_exp+gain_exp)>=100){           
-                levelup  = (my_exp+gain_exp) /100; // 레벨업 숫자
-                gain_exp = (my_exp+gain_exp) % 100; // 남은 exp    
-                printf("[System!] 축하합니다. Level up!\n"); 
-                //레벨업 했을때에만 수치의 변화가 생겨야한다. 그 외에는 변화가 있어선 안된다.
-                
-                my_status[1] = my_status[1] + levelup;
-                my_status[2] = my_status[1] * 10;
-                my_status[3] = my_status[1] *2;
-                my_status[4] = gain_exp;
-              
-            } else {
-                //레벨업을 안한 상태이다.
-                levelup = 0;
-                my_exp = my_exp + gain_exp;         // 경험치만 추가된상태이다.       
-                my_status[4] = my_exp;
-            }
-
-            return my_status;
-
-            break;
-        }else if(my_hp<=0)
-        {
-            // 사실 포션을 이용해서 부활시켜야할거같다.  c_tier 정보도 리셋해야한다.
-            printf("유저 복켓몬이 상대 몬스터에게 %d턴만에 제압당하여 갖고있는 돈과 명예를 모두 잃었습니다 -game over 몬스터의 남은 체력 :%d",turn, mon_hp);
-            // 리셋후 게임재시작은 추후구현
-            printf("게임을 다시 진행하시겠습니까?");
-            scanf("%c", &answer);
-            if(answer='y')
-            {
-                my_status = choose_my_monster();
-                player_die = true;                
-                return my_status;
-            }
-
-            break;
-        }
-
-        turn++;
     }
     return my_status;
 }
@@ -390,3 +424,28 @@ int * choose_my_monster()
 
 }
 
+void call_a_song()
+{
+    //스킬이 있다면 스킬을 수행!  아직은 아무것도 하지 않는다.
+    printf("아쉬운대로 포켓몬 주제가 부릅니다.\n");
+    printf("우리는 모두 친구\n");
+    printf("\n");
+    printf("피카츄 라이츄 파이리 꼬부기\n");
+    printf("버터플 야도란 피죤투 또가스\n");
+    printf("서로 생긴 모습은 달라도 우리는 모두 친구 (맞아)\n");
+    printf("\n");
+    printf("산에서 들에서 때리고 뒹굴고\n");
+    printf("사막에서 정글에서 울다가 웃다가\n");
+    printf("서로 만나기까지 힘들었어도 우리는 모두 친구 (피카피카)\n");
+    printf("\n");
+    printf("울랄랄라 내가 원하는 건 너도 원하고\n");
+    printf("마주 잡은 두 손에 맹세해\n");
+    printf("힘을 내봐 그래 힘을 내봐\n");
+    printf("용기를 내봐 그래 용기를 내봐 \n");
+    printf("\n");
+    printf("피카피카피카츄 피카피카피카츄\n");
+    printf("아름다운 우리 추억 기억해\n");
+    printf("\n");
+    printf("피카피카피카츄 피카피카피카츄\n");
+    printf("너와 나 꿈을 위해\n");
+}
