bb5d11841058699f31b35e81dc16db06cc052bca
sharpscar <sharpscar@gmail.com>
2025-04-04 14:55:21 +0900

    우흥

--- miniproject/a_tp/test_article.c
+++ miniproject/a_tp/test_article.c
@@ -7,7 +7,7 @@ struct article {
     int article_id;
     char article_title[50];
     char article_content[500];
-    char user_id[50];
+    char writer_id[50];
     char reciever_id[50];
     int wrtie_time;
 };
@@ -22,12 +22,76 @@ void write_a_article();
 /*삭제를 하기위해 구조체 배열 변수에 파일의 내용들을 담는 함수*/
 void store_to_article_struct(); 
 
+void delete_a_article_from_articles();
+struct article articles[300]; 
+
+int articles_cnt;
+
 int main()
 {  
+    // ./article.csv 파일의 내용을 구조체 변수에 담는 함수
+    // 호출후 articles라는 구조체 배열에 모두 담는다.
+    store_to_article_struct(); 
+
+    // 6번인덱스를 지우고싶다면 
+    // 이건 삭제를 위한 인덱스 추후 삭제하고자 하는 인덱스를 입력받음
+    int delete_index = 7; 
+    //
+    //삭제하는 내용!
+    for(int i=delete_index; i<30; i++)
+    {
+        articles[i] = articles[i+1];
+    }
+
+   
+
+    // printf("======================삭제후===================================\n");
+    // for (int i=0; i < 20; i++)
+    // {
+    //     printf("인덱스!! %d \n", i);
+    //     printf("글제목 : %s \n",articles[i].article_title);
+    //     printf("글내용 : %s \n",articles[i].article_content);
+        
+    // }
+    //삭제 확인 구조체 배열에서 해당 인덱스가 삭제된것을 확인함.
+
+    //파일에 씁시다! 조심 w는 재앙이야!
+    FILE *file = fopen("./article.csv","w");
+    if(file==NULL)
+    {
+        printf("파일을 열수 없습니다.\n");
+        
+    }
+    //배열의 크기만큼 반복! 지금은 20라인줄정도 되니까 강제로 20번 반복한다고 생각함
+    //추후 articl.csv 행의 크기를 알아오는 변수를 준비하도록!
+    /** 글번호 / 글제목/ 컨텐츠/ 글작성자/ 받는분/ 작성시간  <<-- 입력을 받아서 글등록하도록 변경 예정 */  
+    // fprintf(file, "%d,%s,%s,%s,%s,%d\n",
+    //     articles->article_id,articles->article_title,
+    //     articles->article_content,articles->writer_id,
+    //     articles->reciever_id,articles->wrtie_time
+    // );
+
+    for(int i=0; i<articles_cnt; i++)
+    {
+        fprintf(file, "%d,%s,%s,%s,%s,%d\n",
+        articles[i].article_id,articles[i].article_title,
+        articles[i].article_content,articles[i].writer_id,
+        articles[i].reciever_id,articles[i].wrtie_time);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+void store_to_article_struct()
+{
 
     FILE *file = fopen("./article.csv","r");
     
-    
+    // 구조체 배열을 0으로 초기화한다! - 사용중인 배열의 마지막을 찾기위한 초기화
+    memset(&articles,0,sizeof(articles));
+
+
     char line[256];// 한줄을 저장할 배열
 
     if(file==NULL)
@@ -36,7 +100,6 @@ int main()
         
     }
     //50개의 구조체 배열을 생성했다. 삭제를 위한! 
-    struct article articles[50]; 
     
     int row_count=0;    
     //reading by line
@@ -86,7 +149,7 @@ int main()
             // 파일 토큰에서 작성자를 가져와서
             strcpy(writer,token);
             // 구조체 아티클[행번호].작성자로 넣는다.!
-            strcpy(articles[row_count].user_id, writer);
+            strcpy(articles[row_count].writer_id, writer);
             // printf("작성자입니다 %s\t", writer); //각 필드를 출력
            }
            else if(field_count==4)
@@ -111,31 +174,20 @@ int main()
         }
         row_count++;
         printf("\n");
+        articles_cnt++; //전역 
     }
 
     //구조체에 담는것까진 완성! 3칸이 쓰레기 값이 들어감 
-    for (int i=0; i < row_count; i++)
-    {
-        printf("출력시작!\n");
-        printf("글제목 : %s \n",articles[i].article_title);
-        printf("글내용 : %s \n",articles[i].article_content);
-        printf("출력끝!\n");
-    }
-
-
+    // for (int i=0; i < row_count; i++)
+    // {
+    //     printf("인덱스!! %d \n", i);
+    //     printf("글제목 : %s \n",articles[i].article_title);
+    //     printf("글내용 : %s \n",articles[i].article_content);
+        
+    // }
 
     fclose(file);
-  
 
-
-
-
-    // show_article_list();
-    // write_a_article();
-
-    
-
-    return 0;
 }
 
 void write_a_article()
@@ -154,7 +206,7 @@ void write_a_article()
 
     char user_id[50] = "박경태01";
     // char sender_id[3]= " ";  //없으면 공백으로 쓰자
-    int sender_id =0;
+    char sender_id =" ";
     time_t seconds;
     time(&seconds);
 
@@ -163,14 +215,14 @@ void write_a_article()
     a.article_id = 17;
     // strcpy(a.article_title, "돼지고기를 먹는방법");
     // strcpy(a.article_content, "언제든먹을수 있지");
-    strcpy(a.user_id, user_id);
+    strcpy(a.writer_id, user_id);
     a.wrtie_time = seconds;
 
    
     /** 글번호 / 글제목/ 컨텐츠/ 글작성자/ 받는분/ 작성시간  <<-- 입력을 받아서 글등록하도록 변경 예정 */  
     // fprintf(file, "16,돼지고기를 먹는방법, 언제든먹을수 있지, %s, ,%d, \n", user_id, seconds);  
-    fprintf(file ,"%d, %s,%s,%s,%d, %d",
-         a.article_id,a.article_title, a.article_content,a.user_id,sender_id,a.wrtie_time );
+    fprintf(file ,"%d, %s,%s,%s,%s,%d\n",
+         a.article_id,a.article_title, a.article_content,a.writer_id,sender_id,a.wrtie_time );
 
     fclose(file);
 }
