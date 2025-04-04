c02d82688d646273fbec285cefda301981210642
sharpscar <sharpscar@gmail.com>
2025-04-04 11:07:32 +0900

    왜안되 안돼 ! 맞춤법issue

--- miniproject/a_tp/test_article.c
+++ miniproject/a_tp/test_article.c
@@ -19,11 +19,119 @@ void show_article_list();
 
 void write_a_article();
 
+/*삭제를 하기위해 구조체 배열 변수에 파일의 내용들을 담는 함수*/
+void store_to_article_struct(); 
+
 int main()
 {  
 
+    FILE *file = fopen("./article.csv","r");
+    
+    
+    char line[256];// 한줄을 저장할 배열
+
+    if(file==NULL)
+    {
+        printf("파일을 열수 없습니다.\n");
+        
+    }
+    //50개의 구조체 배열을 생성했다. 삭제를 위한! 
+    struct article articles[50]; 
+    
+    int row_count=0;    
+    //reading by line
+    while(fgets(line, sizeof(line),file))
+    {
+
+        char *token = strtok(line,",");
+        
+        int field_count =0;
+        //reading by column(쉼표를 이용한 필드 )
+        while(token !=NULL)
+        {
+           //글 id
+            if(field_count==0)
+           {
+            int article_id = atoi(token);
+            if(article_id!=0)            
+                // printf("%d\t", article_id); //각 필드를 출력
+                //구조체에 넣자!
+                articles[row_count].article_id = article_id;       
+           }
+           //글제목
+           else if(field_count==1)
+           {
+            char title[50];
+            //파일 토큰에서 글제목을 가져와서
+            strcpy(title,token);
+            // 구조체 아티클[행번호].글제목으로 넣는다!
+            strcpy(articles[row_count].article_title, title);       
+
+           }
+           //글 컨텐츠
+           else if(field_count==2)
+           {
+            char content[50];
+            //파일 토큰에서 글내용을 가져와서
+            strcpy(content,token);
+            // 구조체 아티클[행번호].글내용으로 넣는다!
+            strcpy(articles[row_count].article_content, content);       
+
+            // printf("글내용 :%s\t", content); //각 필드를 출력
+           }
+            // 글작성자 아이디
+           else if(field_count==3)
+           {
+            char writer[50];
+            // 파일 토큰에서 작성자를 가져와서
+            strcpy(writer,token);
+            // 구조체 아티클[행번호].작성자로 넣는다.!
+            strcpy(articles[row_count].user_id, writer);
+            // printf("작성자입니다 %s\t", writer); //각 필드를 출력
+           }
+           else if(field_count==4)
+           {
+            //받는이가 없는 경우도 있습니다 기본값이 0입니다.
+            char reciever[50];
+            // 파일 토큰에서 작성자를 가져와서
+            strcpy(reciever,token);
+            // 구조체 아티클[행번호].작성자로 넣는다.!
+            strcpy(articles[row_count].reciever_id, reciever);
+           }
+           else if(field_count==5)
+           {
+            int write_time;
+            write_time=atoi(token);
+            articles[row_count].wrtie_time = write_time;
+            
+           }
+            token = strtok(NULL, ","); //다음 필드로 이동
+            field_count++;
+            //타이틀만 나오게 하려면 어떻게 해야할까?            
+        }
+        row_count++;
+        printf("\n");
+    }
+
+    //구조체에 담는것까진 완성! 3칸이 쓰레기 값이 들어감 
+    for (int i=0; i < row_count; i++)
+    {
+        printf("출력시작!\n");
+        printf("글제목 : %s \n",articles[i].article_title);
+        printf("글내용 : %s \n",articles[i].article_content);
+        printf("출력끝!\n");
+    }
+
+
+
+    fclose(file);
+  
+
+
+
+
     // show_article_list();
-    write_a_article();
+    // write_a_article();
 
     
 
@@ -45,6 +153,8 @@ void write_a_article()
     // 받은 입력을 구조체에 넣는 함수/ 입력외에 들어가는 정보들 article 마지막id, user_id, time_t
 
     char user_id[50] = "박경태01";
+    // char sender_id[3]= " ";  //없으면 공백으로 쓰자
+    int sender_id =0;
     time_t seconds;
     time(&seconds);
 
@@ -59,7 +169,8 @@ void write_a_article()
    
     /** 글번호 / 글제목/ 컨텐츠/ 글작성자/ 받는분/ 작성시간  <<-- 입력을 받아서 글등록하도록 변경 예정 */  
     // fprintf(file, "16,돼지고기를 먹는방법, 언제든먹을수 있지, %s, ,%d, \n", user_id, seconds);  
-    fprintf(file ,"%d, %s,%s,%s,%d", a.article_id,a.article_title, a.article_content,a.user_id,a.wrtie_time );
+    fprintf(file ,"%d, %s,%s,%s,%d, %d",
+         a.article_id,a.article_title, a.article_content,a.user_id,sender_id,a.wrtie_time );
 
     fclose(file);
 }
